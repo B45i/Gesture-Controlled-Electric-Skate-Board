b39791badb7905ee5145dd6cf18bcd27d27fefb2
b45i <amalshajan2011@gmail.com>
2018-01-18 22:53:26 +0530

    initial commit

---
+++ gesture_robot.ino
@@ -0,0 +1,97 @@
+//Sensor pins
+const int xPin = A0;
+const int yPin = A1;
+const int zPin = A2;
+
+//Motor pins
+const int leftM1  = 9;
+const int leftM2  = 8;
+const int rightM1 = 7;
+const int rightM2 = 6;
+
+//Sensitivity, adjust this for triggering movement
+const int xMinSensitivity = 200;
+const int xMaxSensitivity = 300;
+const int yMinSensitivity = 200;
+const int yMaxSensitivity = 300;
+
+void motorStop() {
+  //Serial.println("Stopping...");
+  digitalWrite(leftM1, LOW);
+  digitalWrite(leftM2, LOW);
+  digitalWrite(rightM1, LOW);
+  digitalWrite(rightM2, LOW);
+}
+
+
+void goForward() {
+  motorStop();
+  Serial.println("Going FWD");
+  digitalWrite(leftM1, HIGH);
+  digitalWrite(leftM2, LOW);
+  digitalWrite(rightM1, HIGH);
+  digitalWrite(rightM2, LOW);
+}
+
+void goLeft() {
+  motorStop();
+  Serial.println("Going Left");
+  digitalWrite(rightM1, HIGH);
+  digitalWrite(rightM2, LOW);
+}
+
+void goRight() {
+  motorStop();
+  Serial.println("Going Right");
+  digitalWrite(leftM1, HIGH);
+  digitalWrite(leftM2, LOW);
+}
+
+void goBackward() {
+  motorStop();
+  Serial.println("Going BWD");
+  digitalWrite(leftM1, LOW);
+  digitalWrite(leftM2, HIGH);
+  digitalWrite(rightM1, LOW);
+  digitalWrite(rightM2, HIGH);
+}
+
+void setup() {
+
+  pinMode(leftM1, OUTPUT);
+  pinMode(leftM2, OUTPUT);
+  pinMode(rightM1, OUTPUT);
+  pinMode(rightM2, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  int xVal = analogRead(xPin);
+  int yVal = analogRead(yPin);
+
+  if(abs(xVal)>abs(yVal)) {
+    if(xVal > xMinSensitivity && xVal < yMaxSensitivity) {
+      motorStop();
+    }
+    else if(xVal > xMaxSensitivity) {
+      goBackward();
+    }
+    else {
+      goForward();
+    }
+  }
+  else {
+    if(yVal > yMinSensitivity && yVal < yMaxSensitivity) {
+      motorStop();
+    }
+    else if(yVal < yMinSensitivity) {
+      goLeft();
+    }
+    else {
+      goRight();
+    }
+  }
+}
+
